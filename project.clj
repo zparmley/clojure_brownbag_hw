52228d09f1811eb2e7fd377370451014cd3676d1
Zachary Parmley <zparmley@sfoeng50-237.clients.corp.yelpcorp.com>
2013-02-06 09:42:51 -0800

    Adding min lein version

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
   				 [ring/ring-jetty-adapter "1.1.0"]]
+  :min-lein-version "2.0.0"
 )
